53c4c069a85e8a7b9e6a3eca11192fa2d937ca59
Abdullah Naveed <abdullah2w1w@gmail.com>
2026-02-20 00:22:45 +0500

    Update evaluate.py

--- Src/evaluate.py
+++ Src/evaluate.py
@@ -7,19 +7,30 @@ from sklearn.metrics import confusion_matrix, classification_report, ConfusionMa
 
 DATA_DIR = "data/final"
 MODEL_PATH = "models/model.pkl"
+FEATURES_PATH = "models/features.pkl"
 ARTIFACT_DIR = "artifacts"
 
 os.makedirs(ARTIFACT_DIR, exist_ok=True)
 
 def evaluate_model():
-
     # load data
     X_test = pd.read_csv(f"{DATA_DIR}/X_test.csv")
     y_test = pd.read_csv(f"{DATA_DIR}/y_test.csv").values.ravel()
 
+    # drop customerID if exists
+    if "customerID" in X_test.columns:
+        X_test.drop(columns=["customerID"], inplace=True)
+
+    # load training feature names
+    feature_names = joblib.load(FEATURES_PATH)
+
+    # reorder X_test columns to match training
+    X_test = X_test[feature_names]
+
     # load model
     model = joblib.load(MODEL_PATH)
 
+    # predictions
     preds = model.predict(X_test)
 
     # confusion matrix
@@ -32,13 +43,11 @@ def evaluate_model():
 
     # classification report
     report = classification_report(y_test, preds)
-
     with open(f"{ARTIFACT_DIR}/classification_report.txt", "w") as f:
         f.write(report)
 
     # log artifacts to MLflow
     mlflow.set_experiment("Telco-Churn")
-
     with mlflow.start_run(run_name="evaluation"):
         mlflow.log_artifact(f"{ARTIFACT_DIR}/confusion_matrix.png")
         mlflow.log_artifact(f"{ARTIFACT_DIR}/classification_report.txt")
